b82a6955c4397b2092acd30d118af0d51ae9ae3e
sam2002jain <jainsam1975@gmail.com>
2024-03-22 22:05:52 +0530

    home done with fragment but content no done yet

--- app/src/main/java/com/example/witlearners/MainActivity.java
+++ app/src/main/java/com/example/witlearners/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.witlearners;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.witlearners.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+   ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replacefragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.home) {
+                replacefragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.search) {
+                replacefragment(new searchFragment());
+            } else if (item.getItemId() == R.id.forum) {
+                replacefragment(new forumFragment());            }
+
+            return true;
+        });
+    }
+
+    private void replacefragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
+
     }
 }
